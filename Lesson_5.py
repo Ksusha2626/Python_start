feeb4bb3bf137606a8f29373e601d2b0672bbaa7
Ksusha Barkovskaya <ksusha2626@gmail.com>
2019-12-22 18:25:18 +0300

    Lesson_5

--- Lesson_5.py
+++ Lesson_5.py
@@ -1,18 +1,10 @@
 """Создать программно файл в текстовом формате, записать в него построчно данные, вводимые пользователем.
 Об окончании ввода данных свидетельствует пустая строка."""
-"""С переносом строки"""
 
 with open('new_file.txt', 'a') as file:
     while True:
         user_answ = input('Write smth: ')
         file.write(user_answ + '\n')
-        if not user_answ:
-            break
-
-"""Без переноса строки"""
-with open('new_file.txt', 'a') as file:
-    while True:
-        user_answ = file.write(input('Write smth: '))
         if not user_answ:
             break
 
@@ -24,10 +16,9 @@ def count_info():
     try:
         with open('new_file.txt', 'r', encoding="utf-8") as file:
             my_list = file.readlines()
-            print(f'Количество строк в файле {len(my_list)}')
             for i in range(len(my_list)):
                 new_l = my_list[i].split()
-                print(f'В {i + 1}-ой строке {len(new_l)} слов(а)')
+                print(f'Количество строк в файле {len(my_list)}. В {i + 1}-ой строке {len(new_l)} слов(а)')
     except FileNotFoundError:
         return 'Файл не найден.'
 
@@ -76,12 +67,12 @@ def rewrite_file():
     new_text = []
     try:
         with open('file.txt', 'r+', encoding="utf-8") as file:
-            l = file.readlines()
-            for i in l:
-                i = i.split(' ', 1)
-                new_text.append(num[i[0]] + ' ' + i[1])
-        with open('one_file.txt', 'r+', encoding="utf-8") as new_file:
-            new_file.writelines(new_text)
+            with open('new_file.txt', 'r+', encoding="utf-8") as new_file:
+                l = file.readlines()
+                for i in l:
+                    i = i.split(' ', 1)
+                    new_text.append(num[i[0]] + ' ' + i[1])
+                new_file.writelines(new_text)
     except FileNotFoundError:
         return 'Файл не найден.'
 
@@ -119,16 +110,17 @@ def count_subjects():
             for line in fobj:
                 name, stats = line.split(':')  # name = Информатика, stats = 100(л) 50(пр) 20(лаб).
                 name_sum = sum(map(int, ''.join([i for i in stats if i == ' ' or ('0' <= i <= '9')]).split()))
-                """1. [i for i in stats if i == ' ' or ('0' <= i <= '9')] - Перебирает все элементы и берет только цифры и пробелы(для разделения цифр: [' ', '1', '0', '0', ' ', '5', '0', ' ', '2', 
-                 '0']) 
+                """1. [i for i in stats if i == ' ' or ('0' <= i <= '9')] - Перебирает все элементы и берет только цифры
+                        и пробелы(для разделения цифр: [' ', '1', '0', '0', ' ', '5', '0', ' ', '2', '0'])
 
-                 2. ''.join([i for i in stats if i == ' ' or ('0' <= i <= '9')]) с помощью join объединяет 
-                 получившееся: _100_50_20  (где _ это пробел) 
+                 2. ''.join([i for i in stats if i == ' ' or ('0' <= i <= '9')]) с помощью join объединяет
+                        получившееся: _100_50_20  (где _ это пробел)
 
-                 3. ''.join([i for i in stats if i == ' ' or ('0' <= i <= '9')]).split() - делит по пробелам методом .split(): ['100', '50', '20'] 
+                 3. ''.join([i for i in stats if i == ' ' or ('0' <= i <= '9')]).split()
+                        - делит по пробелам методом .split(): ['100', '50', '20']
 
-                 4. map(int, ''.join([i for i in stats if i == ' ' or ('0' <= i <= '9')]).split()) 
-                 - с помощью map(int, ['100', '50', '20']) приводит все элементы списка к типу int 
+                 4. map(int, ''.join([i for i in stats if i == ' ' or ('0' <= i <= '9')]).split())
+                        - с помощью map(int, ['100', '50', '20']) приводит все элементы списка к типу int
 
                  5. с помощью sum(['100', '50', '20']) суммирует все элементы списка """
                 mydict[name] = name_sum
